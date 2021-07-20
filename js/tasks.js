7039575eaebe82191216ebe7949208d5b4a5b0c2
Ziek <zieksanchez3@gmail.com>
2021-07-20 12:15:14 -0500

    added grayscale filter to add button

---
+++ js/tasks.js
@@ -0,0 +1,27 @@
+const addTask = document.getElementById('add');
+const body = document.querySelector('html');
+
+//add task (store tasks as well)
+
+
+//show tasks with event listener to delete (call when updating display)
+
+//find alternative to display none problem 
+
+addTask.addEventListener('click', () => {
+
+    displayAddScreen();
+
+
+
+
+
+});
+
+
+function displayAddScreen() {
+
+    body.style.filter = 'grayscale(50%)';
+
+
+}
